157c0e504bb8170d47b4a91e6879cebb964f51d1
Sevdenur Yılmaz <75967411+seevdey@users.noreply.github.com>
2023-02-03 23:01:38 +0300

    Add files via upload

---
+++ WinForm18/WinForm18/Form3.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace WinForm18
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(34, 119);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 548);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(722, 119);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(302, 548);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(493, 202);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(154, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(499, 262);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(354, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CustomerID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(339, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CompanyName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(71, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Müşteriler İlk Durum";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(730, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(279, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Müşteriler Son Durum(ListBox)";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Peru;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(436, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 50);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "GÜNCELLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Khaki;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(436, 422);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 57);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "GÖSTER";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(1110, 119);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(341, 548);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(1134, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(288, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Müşteriler Son Durum(ListView)";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(1502, 728);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ WinForm18/WinForm18/Form3.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm18
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        //Var olan dataları değiştirmek için Disconnected kullanır
+
+        /*
+         * 1. listboxta müşteriler tablosu var. 
+         * 1. listbox ta herhangi bir veriye 2 kez tıklayınca seçilen verileri textboxa yazdırır
+         * sadece companyname kısmı değiştirilebilir
+         * değiştirdikten sonra update butonuna bas 
+         * değişikliği göstermek için göster butonuna bas
+        */
+        SqlConnection conn = new SqlConnection("Server = SEVDE; Database=Northwind;Trusted_Connection=true;");
+        SqlDataAdapter dap;
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+            dap = new SqlDataAdapter("select CustomerId, CompanyName from Customers", conn);
+
+            DataSet ds = new DataSet();
+            dap.Fill(ds);
+
+            listBox1.DataSource = ds.Tables[0];
+            listBox1.DisplayMember = "CompanyName";
+            listBox1.ValueMember = "CustomerId";
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            textBox1.Text = listBox1.SelectedValue.ToString();
+
+            textBox2.Text = listBox1.GetItemText(listBox1.SelectedItem);
+
+            //textbox2 yi doldurma -- 2. YOL
+            DataRowView drv = (DataRowView)listBox1.SelectedItem;
+            textBox2.Text = drv["CompanyName"].ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //UPDATE
+        {
+            SqlCommand cmd = new SqlCommand("UPDATE Customers set CompanyName = @Cname where CustomerID = @id", conn);
+          
+            dap.UpdateCommand = cmd;
+
+            dap.UpdateCommand.Parameters.AddWithValue("@id", textBox1.Text);
+            dap.UpdateCommand.Parameters.AddWithValue("@Cname", textBox2.Text);
+
+            conn.Open();
+            dap.UpdateCommand.ExecuteNonQuery();
+            MessageBox.Show("İşlem başarılı");
+            conn.Close();
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             dap = new SqlDataAdapter("select CustomerId, CompanyName from Customers", conn);
+
+            DataSet ds = new DataSet();
+            dap.Fill(ds);
+
+            listBox2.DataSource = ds.Tables[0];
+            listBox2.DisplayMember = "CompanyName";
+            listBox2.ValueMember = "CustomerId";
+
+            //Kolon başlıklarını oluşturma
+            listView1.Columns.Add("Id");
+            listView1.Columns.Add("Name",150); //genişliğini 150 yap
+
+            //listView1.Columns[1].Width = 150;
+
+            //listbox2 deki tüm kayıtları ekleme
+            //foreach (DataRow item in ds.Tables[0].Rows)
+            //{
+            //    ListViewItem lvi = new ListViewItem();
+            //    lvi.Text = item["CustomerId"].ToString();
+            //    lvi.SubItems.Add(item["CompanyName"].ToString());
+            //    listView1.Items.Add(lvi);
+            //}
+        }
+
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            //seçili listbox2 kaydını listview a ekleme
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = listBox2.SelectedValue.ToString();
+            DataRowView drv = (DataRowView)listBox2.SelectedItem;
+            lvi.SubItems.Add(drv["CompanyName"].ToString());
+            listView1.Items.Add(lvi);
+        }
+    }
+}
